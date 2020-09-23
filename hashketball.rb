949ad3e83133af0e7c50edbb07ce457783cde6f7
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-23 00:14:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -180,7 +180,7 @@ def player_stats(name)
   game_hash.each do |both_teams, keys|
     keys[:players].each do |player|
       if player[:player_name] == name
-        return player.delete_if { |stat, value| [:player_name].include?(stat)}
+        return player[:player_name]
       end
     end
   end
